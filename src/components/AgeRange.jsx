ba752a2644b21066b73406046a4a372da088052f
fdavison1 <fred.noreply@gmail.com>
2020-03-19 11:43:53 -0500

    WIP

--- src/components/AgeRange.jsx
+++ src/components/AgeRange.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AgeRange = () => {
     const ageRange = ['0-49', '50-59', '60-75', '76+'];
 
-    return(
+    const state = {
+        '0-49': 'btn-primary', 
+        '50-59': 'btn-primary', 
+        '60-75': 'btn-primary', 
+        '76+': 'btn-primary'
+    }
+    
+
+    const [buttonDisplay, changeButtonDisplay] = useState(state);
+
+    const onButtonClick = e => {
+        let button = e.target.name;
+        console.log(button);
+        changeButtonDisplay(buttonDisplay[button] === 'btn-primary' ? 'btn-default' : 'btn-primary');
+        console.log(buttonDisplay);
+    }
+
+    return (
         <div>
             <h4>Age Range</h4>
             {ageRange.map(range => {
                 return (
-                    <button>
+                    <button
+                        key={range}
+                        name={range}
+                        className={buttonDisplay[range]}
+                        onClick={onButtonClick}
+                    >
                         {range}
                     </button>
                 )
